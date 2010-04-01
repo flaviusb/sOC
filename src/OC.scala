46c2c60da0d233965c0bfbf45dab8a9edc422876
Justin Marsh (:flaviusb) <justinius.marsh@gmail.com>
2010-04-01 16:10:29 +1300

    First commit of the mockup, about three days into learning Scala.

---
+++ src/OC.scala
@@ -0,0 +1,101 @@
+import _root_.org.eclipse.jface._
+import _root_.org.eclipse.swt._
+import org.eclipse.jface.action._
+import org.eclipse.swt.custom._
+import org.eclipse.swt.graphics._
+import org.eclipse.swt.layout._
+import org.eclipse.swt.widgets._
+import org.eclipse.jface.window._
+import org.eclipse.jface.viewers._
+
+case class MI1(title: String, action: Unit => Unit, children: scala.List[MI1]) { }
+object MI1 {
+  def apply(title: String, children: scala.List[MI1]): MI1 = apply(title, Unit => println("Does nothing"), children)
+}
+
+case class MI2(parent: MenuManager, title: String, children: scala.List[MI1]) {
+  children foreach(x => {
+    if (x.children.length == 0) { 
+      var menuitem = new Actioned(x.action)
+      menuitem.setText(x.title)
+      parent.add(menuitem)
+    } else {
+      var menu = new MenuManager(x.title);
+      MI2(menu, x.title, x.children)
+      parent.add(menu)
+    }
+  })
+}
+
+case class MB(children: scala.List[MI1]) {
+  var me = new MenuManager("")
+  children foreach(x => {
+    if (x.children.length == 0) { 
+      var menuitem = new Actioned(null)
+      menuitem.setText(x.title)
+      me.add(menuitem)
+    } else {
+      var menu = new MenuManager(x.title);
+      MI2(menu, x.title, x.children)
+      me.add(menu)
+    }
+  })
+}
+
+class Actioned(toRun: Unit => Unit) extends Action {
+  override def run = {
+    if (toRun != null)
+      toRun()
+  }
+}
+
+object OC {
+  class sOCWindow extends ApplicationWindow(null) {
+    implicit def MI12List(a: MI1) : scala.List[MI1] = { a :: Nil }
+    addMenuBar()
+    def openAboutBox = {
+      println ("foo")
+    }
+    protected override def createMenuManager() : MenuManager = {
+      var menu = MB(
+        MI1("File", 
+          MI1("New", Unit => println("New Model not implemented") , Nil) ::
+          MI1("Open", Unit => println("Open Model not implemented") , Nil) ::
+          MI1("Exit", Nil)
+        ) ::
+        MI1("Edit", Nil) ::
+        MI1("Help", MI1("About", Unit => openAboutBox , Nil))
+      ).me
+      menu
+    }
+    protected override def createContents(parent: Composite) : Control = {
+      parent.setLayout(new GridLayout())
+      //var ctabf = new CTabFolder(parent, SWT.BORDER);
+      var j = new CTabFolder(parent, SWT.MULTI)
+      for(i<- 1 to 8) {
+        j = new CTabFolder(parent, SWT.CLOSE)
+        var k = new CTabItem(j, SWT.CLOSE | SWT.MULTI)
+        k.setText(i.toString())
+        var foo = new org.eclipse.swt.widgets.Text(j, SWT.BORDER|SWT.MULTI)
+        foo.setText("Contents of pane " + i.toString())
+        k.setControl(foo)
+        null
+      }
+      parent
+    }
+  }
+  def main(args : Array[String]) : Unit = {
+    var demo = new sOCWindow()
+    demo.setBlockOnOpen(true)
+    demo.open()
+    Display.getCurrent().dispose()
+  }
+}
+
+
+
+
+
+
+
+
