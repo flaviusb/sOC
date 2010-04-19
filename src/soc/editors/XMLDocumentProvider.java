70d8f8903e0268ceb03b143ec357c41ec8ec3ca7
Justin Marsh (:flaviusb) <justinius.marsh@gmail.com>
2010-04-19 12:42:37 +1200

    Add syntax highlighting to the xml editor.

--- src/soc/editors/XMLDocumentProvider.java
+++ src/soc/editors/XMLDocumentProvider.java
@@ -1,16 +1,24 @@
 package soc.editors;
 
+import org.eclipse.core.filebuffers.IDocumentSetupParticipant;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.IDocumentPartitioner;
 import org.eclipse.jface.text.rules.FastPartitioner;
 import org.eclipse.ui.editors.text.FileDocumentProvider;
+import org.eclipse.ui.editors.text.ForwardingDocumentProvider;
 import org.eclipse.ui.editors.text.TextFileDocumentProvider;
 
-public class XMLDocumentProvider extends TextFileDocumentProvider {
+public class XMLDocumentProvider extends ForwardingDocumentProvider {
+    public XMLDocumentProvider()
+    {
+      super(null, new innersetup(), new TextFileDocumentProvider());	
+    }
 
-	/*protected IDocument createDocument(Object element) throws CoreException {
-		IDocument document = super.createDocument(element);
+}
+class innersetup implements IDocumentSetupParticipant {
+	public void setup(IDocument document) {
+		//IDocument document = super.createDocument(element);
 		if (document != null) {
 			IDocumentPartitioner partitioner =
 				new FastPartitioner(
@@ -21,6 +29,5 @@ public class XMLDocumentProvider extends TextFileDocumentProvider {
 			partitioner.connect(document);
 			document.setDocumentPartitioner(partitioner);
 		}
-		return document;
-	} */
+	} 
 }
